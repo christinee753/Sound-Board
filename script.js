74196eb7e4cd28a66cbe6759b37d3a36b868d7f6
Kelly Trueheart <kellytrueheart@Kellys-MacBook-Air.local>
2026-09-04 19:32:08 -0400

    Add stylistic changes

--- script.js
+++ script.js
@@ -1,6 +1,15 @@
 // Create an array of sounds
 const sounds = ['applause', 'boo', 'gasp', 'tada', 'victory', 'wrong'];
 
+const emojis = {
+    applause: '👏',
+    boo: '👎',
+    gasp: '😮',
+    tada: '🎉',
+    victory: '🏆',
+    wrong: '❌'
+};
+
 // For each sound, create a button, add a class 'btn', update the text, append to DOM
 sounds.forEach((sound) => {
     // create a button
@@ -10,8 +19,26 @@ sounds.forEach((sound) => {
     btn.classList.add('btn');
 
     // set the button label text to be the same as the string value in the sounds array
-    btn.innerText = sound;
+    btn.innerText = `${emojis[sound]} ${sound}`;
+
+    //Add an event listener to this button
+    btn.addEventListener('click', () => {
+        // Stop all sounds first
+        stopSounds();
+        document.getElementById(sound).play();
+    });
 
     // Add the button to the DOM
     document.getElementById('buttons').appendChild(btn);
 });
+
+// Helper fuctions
+function stopSounds() {
+    sounds.forEach((sound) => {
+        const snd = document.getElementById(sound);
+
+        snd.pause();
+        snd.currentTime = 0;
+    });
+}
+
